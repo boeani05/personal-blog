50d677f98e4ad457dca83fe061c18f195dc80504
boeani05 <flyingdoge@gmx.at>
2026-03-02 16:55:20 +0100

    Can now show specific article as guest user

--- src/main/java/com/bernhard/personal_blog/controller/guest/GuestController.java
+++ src/main/java/com/bernhard/personal_blog/controller/guest/GuestController.java
@@ -1,14 +1,12 @@
 package com.bernhard.personal_blog.controller.guest;
 
 import com.bernhard.personal_blog.entity.Article;
-import com.bernhard.personal_blog.repo.ArticleRepository;
 import com.bernhard.personal_blog.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GuestController {
@@ -24,7 +22,8 @@ public class GuestController {
 
     @GetMapping("/article/{id}")
     public String articles(
-            @PathVariable Long id, Model model
+            @PathVariable Long id,
+            Model model
     ) {
         Article article = articleService.findById(id);
         model.addAttribute("article", article);
